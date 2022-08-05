fc83f9ba6865c93a96e4c0855eb628f67a8bc79c
Jordan Prevot <jordan.prevot59@gmail.com>
2022-08-05 16:21:28 +0200

    function search OK

--- js/home.js
+++ js/home.js
@@ -6,6 +6,26 @@ import { GetProductQuery } from "./get-product-query.js";
 const $dropdownMenu = document.querySelector(".dropdown-menu");
 const $productCards = document.querySelector("#product-cards");
 //<ul class="dropdown-menu"></ul>
+const submitBTN = document.querySelector("#submit_btn");
+const userResearch = document.querySelector("#research");
+
+submitBTN.addEventListener("click", function(event)
+{
+    console.log(submitBTN);
+    event.preventDefault();
+
+    $productCards.innerHTML = "";
+
+    axios.get(`${BASE_URL}/products/search?searchTerm=${userResearch.value}`)
+    .then((response)=>
+    {
+        const products =response.data;
+        for(let product of products){
+            const $cardItem = CreateProductCard(product);
+            $productCards.appendChild($cardItem);
+        }
+    })
+})
 
 axios.get(`${BASE_URL}/categories`).then(function (response){
     const categories = response.data;
@@ -25,3 +45,4 @@ axios.get(`${BASE_URL}/products${productQuery}`).then((response)=>{
         $productCards.appendChild($cardItem);
     }
 })
+
